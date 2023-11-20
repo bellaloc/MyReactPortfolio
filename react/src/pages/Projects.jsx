1f765eeabad8574eea439a33e72608f7dab6ea8d
Christa Lococo <montecitocandleco@gmail.com>
2023-11-20 15:18:21 -0700

    changes

---
+++ react/src/pages/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <div>
+      <h1>View My Projects</h1>
+      <p>
+        Explore my portfolio to see the projects I've worked on during my time at the UCLA Extension Bootcamp. From dynamic web applications to responsive websites, each project reflects my dedication to quality and innovation. Let's turn ideas into reality!
+      </p>
+    </div>
+  );
+};
+
+export default Projects;
