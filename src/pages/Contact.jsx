180ba1a8c9c2a83f79e6c309ba7a1a63e2c2edd1
Christa Lococo <montecitocandleco@gmail.com>
2023-11-22 10:52:14 -0700

    files

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Me</h1>
+      <p>
+        Ready to take your project to the next level? Feel free to reach out! I am open to collaboration and always excited about new opportunities. You can contact me via email at christa.lococo@example.com or connect with me on LinkedIn [insert LinkedIn profile link].
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the FullStack Portfolio of Christa Lococo</h1>
+      <p>
+        Hi, I'm Christa Lococo, a passionate Full Stack Developer who graduated from the UCLA Extension Bootcamp. I bring a solid foundation in web development and a drive for creating innovative solutions. Let's build something amazing together!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
